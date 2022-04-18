ff00e81ffe943a177430f00755547f1f128a5505
Tigran Zalian <tigraanzalian@gmail.com>
2022-04-18 11:14:52 +0300

    Added unhandled_exception property to error class

--- src/exception_handler/errors.py
+++ src/exception_handler/errors.py
@@ -5,4 +5,7 @@ class UnhandledExceptionError(Exception):
     def __init__(self, ex: Exception, expected_ex: Type[Exception]) -> None:
         msg =  f"\n> Expected '{expected_ex.__name__}' and its subclasses to handle, but got '{ex.__class__.__name__}' instead."
         msg += f"\n> '{ex.__class__.__name__}' is not subclass of '{expected_ex.__name__}'."
+        
+        self.unhandled_exception = ex
+        
         super().__init__(msg)
